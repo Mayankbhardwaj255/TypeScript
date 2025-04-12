3909caf1f0470ba56f5182fc4605a7fabdac30f2
Mayankbhardwaj255 <mayankbhardwaj708@gmail.com>
2025-04-13 00:01:16 +0530

    Arrays in ts

---
+++ 02basics/myArray.js
@@ -0,0 +1,16 @@
+// const superHeros = [] //type is "never";
+// superHeros.push("spiderman"); //string is not assignable to parameter type "never"
+var superHeros = []; // it means the superHeros is array of string type. 
+superHeros.push("spiderman");
+superHeros.push("Ironman");
+var heroPower = []; // it means the heroPower is array of number type.
+heroPower.push(2);
+//another method of declaratrion:-
+var hero = [];
+hero.push(1);
+var allUsers = [];
+allUsers.push({ name: "mayank", isActive: false });
+var MLModels = [
+    [255, 255, 255],
+    [1, 1, 1]
+];
